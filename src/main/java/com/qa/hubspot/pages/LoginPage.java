b6c032d9b0a598688f68d5570c06ce6646cff08d
smitatech17 <smitansharma@gmail.com>
2020-05-05 14:52:19 -0500

    this is first commit for POM framework

---
+++ src/main/java/com/qa/hubspot/pages/LoginPage.java
@@ -0,0 +1,55 @@
+package com.qa.hubspot.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.qa.hubspot.base.BasePage;
+import com.qa.hubspot.utils.Constants;
+import com.qa.hubspot.utils.ElementUtil;
+
+//import io.qameta.allure.Step;
+
+public class LoginPage extends BasePage {
+	
+	WebDriver driver;
+	ElementUtil elementUtil;
+
+	//1. declare all By Locators:
+	By username = By.id("username");
+	By password = By.id("password");
+	By loginButton = By.id("loginBtn");
+	By SignUpLink = By.linkText("Sign up");
+
+	
+	//2. constructor of the page class:
+	public LoginPage(WebDriver driver) {
+		this.driver = driver;
+		elementUtil = new ElementUtil(driver);
+	}
+
+	
+	//3. Page actions/methods:
+	//@Step is for pages and @Descrption is for test cases. @Step and @Description comes from allure
+//	@Step("get login page title...")
+	public String getLoginPageTitle() {
+		 return elementUtil.waitForTitleToBePresent(Constants.LOGIN_PAGE_TITLE, 10);
+	}
+	
+//	@Step("check sign up link on login page...")
+	public boolean checkSignUpLink() {
+		return elementUtil.doIsDisplayed(SignUpLink);
+	}
+	
+//	@Step("login with - username : {0} and password : {1}")
+	public HomePage doLogin(String un, String pwd) {
+		elementUtil.doSendKeys(username, un);
+		elementUtil.doSendKeys(password, pwd);
+		elementUtil.doClick(loginButton);
+		
+		//whenever we clicking on the login button, we are landing on the home page and According to POM, whenever we are clicking on the button or link and landing on some other page, that method should return landing page object
+		return new HomePage(driver);
+	}
+	
+	
+	
+}
---
+++ src/test/java/com/qa/hubspot/tests/LoginPageTest.java
@@ -0,0 +1,74 @@
+package com.qa.hubspot.tests;
+
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.qa.hubspot.base.BasePage;
+import com.qa.hubspot.pages.HomePage;
+import com.qa.hubspot.pages.LoginPage;
+import com.qa.hubspot.utils.Constants;
+
+//import io.qameta.allure.Description;
+//import io.qameta.allure.Epic;
+//import io.qameta.allure.Feature;
+//import io.qameta.allure.Severity;
+//import io.qameta.allure.SeverityLevel;
+
+	//pre condition ---> Test ---> actual result Vs expected Result ----> post step
+	//@BeforeTest ---> @Test ----> Assert ---> @AfterTest
+	//browser, url --->test google title ----> Google Vs Google -----> close browser
+
+//@Epic("Epic - 101 : design login feature")
+//@Feature("US - 105 : design test cases for login page feature")
+public class LoginPageTest {
+	
+	Properties prop;
+	WebDriver driver;
+	
+	BasePage basePage;
+	LoginPage loginPage;
+	
+	@BeforeTest
+	public void setUp() {
+		
+		basePage = new BasePage();
+		prop = basePage.init_prop();
+		driver = basePage.init_driver(prop);
+		loginPage = new LoginPage(driver);
+	}
+	
+	@Test(priority=1, enabled=true)
+//	@Description("verify Login Page Title Test....")
+//	@Severity(SeverityLevel.NORMAL)
+	public void verifyLoginPageTitleTest() {
+		String title = loginPage.getLoginPageTitle();
+		System.out.println("login page title is : " + title);
+		Assert.assertEquals(title, Constants.LOGIN_PAGE_TITLE,"title is not found....");
+	}
+	
+	@Test(priority=2)
+//	@Description("verify sign up link test...")
+//	@Severity(SeverityLevel.CRITICAL)
+	public void verifySgnUpLinkTest() {
+		Assert.assertTrue(loginPage.checkSignUpLink(),"sign up link is not present....");
+	}
+	
+	@Test(priority=3)
+//	@Description("verify user is able to login - feature test...")
+//	@Severity(SeverityLevel.BLOCKER)
+	public void loginTest() {
+		HomePage homePage = loginPage.doLogin(prop.getProperty("username"), prop.getProperty("password"));
+		Assert.assertEquals(homePage.getAccoutName(), prop.getProperty("accountname"),"login is failed....");
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+	}
+	
+}
